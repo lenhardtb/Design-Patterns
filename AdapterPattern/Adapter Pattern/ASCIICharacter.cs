301873838e0c79864a336aa4e20c19e1ac78de5a
lenhardtb <b-lenhardt@onu.edu>
2017-10-27 09:19:32 -0400

    Add files via upload

---
+++ AdapterPattern/Adapter Pattern/ASCIICharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Pattern
+{
+    public class ASCIICharacter : BinaryCharacter
+    {
+        //Value is already in BinaryCharacter
+
+        public ASCIICharacter() :base() { }
+        public ASCIICharacter(int i) :base(i){ }
+
+        public override string WrittenForm()
+        {
+            return "" + (char)(Value);
+        }
+    }
+}
---
+++ AdapterPattern/Adapter Pattern/BinaryCharacter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Pattern
+{
+    public class BinaryCharacter : Character
+    {
+        private int value;
+        public int Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                if (value > 255 || value < 0) throw new ArgumentException("Character value must be between 0-255");
+                this.value = value;
+            }
+        }
+
+        public BinaryCharacter() { }
+        public BinaryCharacter(int value) { Value = value; }
+
+        public override String WrittenForm()
+        {
+            String flippedBinaryDigits = "";
+            int val = Value;
+
+            while(val > 0)
+            {
+                flippedBinaryDigits += val % 2;
+                val = val / 2;
+            }
+
+            String returner = (String)(flippedBinaryDigits.Reverse());
+
+            return returner;
+        }
+
+        public static implicit operator int(BinaryCharacter b)
+        {
+            return b.Value;
+        }
+
+        public static implicit operator BinaryCharacter(int i)
+        {
+            return new BinaryCharacter(i);
+        }
+    }
+}
